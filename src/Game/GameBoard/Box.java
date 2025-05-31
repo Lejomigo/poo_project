9d0cfb08d370bc91a2525028939bce36e1a0d0dd
Lejomigo <leopoldomicettgonzalez@gmail.com>
2025-05-31 01:52:41 -0400

    completed Board and prepared the pieces to start the game.

--- src/Game/GameBoard/Box.java
+++ src/Game/GameBoard/Box.java
@@ -8,7 +8,35 @@ import java.util.List;
 public class Box {
     CategoryColors color;
     public int number;
-    List<Piece> pieces;
+    private List<Piece> pieces;
 
+    public Box(CategoryColors color, int number, List<Piece> pieces) {
+        setColor(color);
+        setNumber(number);
+        setPieces(pieces);
+    }
 
+    public void setColor(CategoryColors color) {
+        this.color = color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setPieces(List<Piece> pieces) {
+        this.pieces = pieces;
+    }
+
+    public CategoryColors getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public List<Piece> getPieces() {
+        return pieces;
+    }
 }
